7f7918cd031305877a81a3bfc6c1610cdb1662bc
nsart75 <nsart75@gmail.com>
2023-06-10 22:34:24 +0330

    first commit

---
+++ project.js
@@ -0,0 +1,161 @@
+
+const prompt = require('prompt-sync')();
+
+const ROWS= 3;
+const COLS = 3 ;
+
+const   SYMBOLS_COUNT = {
+    "A": 2 ,
+    "B": 4 ,
+    "C": 6 ,
+    "D": 8
+};
+
+const SYMBOLS_VALUES = {
+    "A" : 5 , 
+    "B" : 4 , 
+    "C" : 3 ,
+    "D" : 2
+};
+
+
+const deposit = () =>{
+    while (true) {
+
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberdepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberdepositAmount) || numberdepositAmount<= 0 ) {
+            console.log("Invalid deposit amount, try again.")
+        }
+        else {
+            return numberdepositAmount
+        }
+    }
+};
+
+const getNumberOfLines = ()=>{
+    while (true) {
+
+        const Lines  = prompt("Enter the number of lines to bet on (3-1): ");
+        const numberOfLines = parseFloat(Lines );
+
+        if (isNaN(numberOfLines) || numberOfLines<= 0 || numberOfLines > 3 ) {
+            console.log("Invalid number of line, try again.")
+        }
+        else {
+            return numberOfLines
+        }
+    }
+};
+
+const getBet=(balance , lines)=>{
+    while (true) {
+
+        const bet  = prompt("Enter the bet per line: ");
+        const numberbet = parseFloat(bet );
+
+        if (isNaN(numberbet) || numberbet<= 0 || numberbet > balance/lines)  {
+            console.log("Invalid bet, try again.")
+        }
+        else {
+            return numberbet
+        }
+    }
+};
+
+const spin =() =>{
+    const symbols =[];
+    for (const [symbol , count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0 ; i < count ; i++  ){
+            symbols.push(symbol);
+        }
+    }
+    const reels = [];
+    for (let i = 0 ; i< COLS ; i++ ){
+        reels.push([]);
+        const reelsymbols  = [...symbols];
+        for (let j = 0 ; j <ROWS ; j++) {
+                const randomIndex = Math.floor(Math.random()* reelsymbols.length)
+                    const selectedSymbol = reelsymbols[randomIndex];
+                    reels[i].push(selectedSymbol);
+                    reelsymbols.splice(randomIndex , 1);
+        };
+    }
+    return reels;
+};
+
+const traspose =(reels) =>{
+    const rows = [];
+    for (let i = 0 ; i < ROWS; i++ ){
+        rows.push([]);
+        for (let j = 0 ; j < COLS ; j++){
+            rows[i].push(reels[j][i])
+        }
+    }
+    return rows;
+};
+
+const printRows = (rows) =>{
+    for (const row of rows){
+        let rowString ="";
+        for(const [i , symbol] of row.entries()){
+            rowString += symbol
+            if (i != row.length-1){
+                rowString += " | "
+            }
+        }
+        console.log(rowString)
+    }
+};
+
+const getWinnings = (rows , bet , lines )=>{
+    let winnings = 0 ; 
+    for(let row = 0 ; row <lines ; row++ ){
+        const symbols = rows[row];
+        let allSame= true ;
+        
+        for(const symbol of symbols){
+            if (symbol !=symbols[0] ){
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame){
+            winnings +=bet * SYMBOLS_VALUES[symbols[0]]       
+        }
+    }
+    return winnings
+};
+
+const game =()=>{
+    let   balance = deposit();
+
+    while(true){
+        console.log("You have a balance of $ " + balance)
+        const   numberOfLines = getNumberOfLines();
+        const   bet = getBet(balance , numberOfLines)
+        balance -= bet * numberOfLines;
+        const reels = spin();
+        const rows = traspose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows , bet ,numberOfLines)
+        balance += winnings
+        console.log("You won, $" +winnings.toString())
+
+        if (balance <= 0){
+            console.log("You ran out of money")
+            break;
+        }
+
+        const playAgain = prompt("Do you want to play again?(Y,N)") 
+        if (playAgain != "Y")break;
+    }
+};
+
+game();
+
+
+
+
